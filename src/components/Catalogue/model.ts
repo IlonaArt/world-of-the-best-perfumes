18c0e064733397da35e3be01de3be547ac0ecd5f
IlonaArt <ilonasurdream@gmail.com>
2024-04-21 14:19:21 +0200

    moved sort function to model file

--- src/components/Catalogue/model.ts
+++ src/components/Catalogue/model.ts
@@ -1,14 +1,54 @@
-import { createEffect, StoreWritable, createStore, sample } from 'effector'
+import {
+  createEffect,
+  StoreWritable,
+  createStore,
+  sample,
+  createEvent,
+  attach,
+} from 'effector'
 import { createGate } from 'effector-react'
-import { Perfume } from '../../interfaces'
+import { Perfume, SortType } from '../../interfaces'
 import { fetchAllPerfume } from './fetchAllPerfume'
 
 export const PerfumeGate = createGate()
 
+export const dataSorted = createEvent<SortType>()
+
 export const fetchDataSideEffect = createEffect(fetchAllPerfume)
+const sort = createEffect(
+  ({ data, sortType }: { data: Perfume[]; sortType: SortType }) => {
+    const newSortedData = [...data].sort((perfume1, perfume2) => {
+      const brand1 = perfume1.brand.toUpperCase()
+      const brand2 = perfume2.brand.toUpperCase()
+      const finalPrice1 =
+        perfume1.discount.length > 0 ? perfume1.discount[0] : perfume1.price[0]
+      const finalPrice2 =
+        perfume2.discount.length > 0 ? perfume2.discount[0] : perfume2.price[0]
+
+      if (sortType.includes('alphabet')) {
+        return brand1 < brand2 ? -1 : 1
+      } else if (sortType.includes('price')) {
+        return finalPrice1 - finalPrice2
+      }
+      return 0
+    })
+
+    if (sortType.includes('desc')) {
+      newSortedData.reverse()
+    }
+
+    return newSortedData
+  },
+)
 
 export const $data: StoreWritable<Perfume[]> = createStore(null)
+export const $sortedData: StoreWritable<Perfume[]> = createStore(null)
 export const $error = createStore(null)
+const sortDataEffect = attach({
+  source: $data,
+  effect: sort,
+  mapParams: (sortType: SortType, data) => ({ data, sortType }),
+})
 
 $data.on(fetchDataSideEffect.doneData, (_, data) => data)
 $error.on(fetchDataSideEffect.failData, (_, error) => error.message)
@@ -16,4 +56,20 @@ $error.on(fetchDataSideEffect.failData, (_, error) => error.message)
 sample({
   clock: PerfumeGate.open,
   target: fetchDataSideEffect,
+})
+
+sample({
+  clock: $data,
+  fn: (): SortType => 'asc-alphabet',
+  target: dataSorted,
+})
+
+sample({
+  clock: dataSorted,
+  target: sortDataEffect,
+})
+
+sample({
+  clock: sortDataEffect.doneData,
+  target: $sortedData,
 })
--- src/pages/catalogue.tsx
+++ src/pages/catalogue.tsx
@@ -4,48 +4,21 @@ import PerfumeList from '../components/Catalogue/PerfumeList/PerfumeList'
 import Filters from '../components/Catalogue/Filters/Filters'
 import { useGate, useUnit } from 'effector-react'
 import {
-  $data,
   $error,
+  $sortedData,
+  dataSorted,
   PerfumeGate,
   fetchDataSideEffect,
 } from '../components/Catalogue/model'
-import { useEffect, useState } from 'react'
-import { Perfume, SortType } from '../interfaces'
 
 const CataloguePage = () => {
   useGate(PerfumeGate)
-  const [data, error, loading] = useUnit([$data, $error, fetchDataSideEffect.pending])
-  const [sortedData, setSortedData] = useState<Perfume[] | null>(null)
-
-  const handleSort = (sortType: SortType) => {
-    const newSortedData = [...data].sort((perfume1, perfume2) => {
-      const brand1 = perfume1.brand.toUpperCase()
-      const brand2 = perfume2.brand.toUpperCase()
-      const finalPrice1 =
-        perfume1.discount.length > 0 ? perfume1.discount[0] : perfume1.price[0]
-      const finalPrice2 =
-        perfume2.discount.length > 0 ? perfume2.discount[0] : perfume2.price[0]
-
-      if (sortType.includes('alphabet')) {
-        return brand1 < brand2 ? -1 : 1
-      } else if (sortType.includes('price')) {
-        return finalPrice1 - finalPrice2
-      }
-      return 0
-    })
-
-    if (sortType.includes('desc')) {
-      newSortedData.reverse()
-    }
-
-    setSortedData(newSortedData)
-  }
-
-  useEffect(() => {
-    if (!data) return
-
-    handleSort('asc-alphabet')
-  }, [loading])
+  const [sortedData, sort, error, loading] = useUnit([
+    $sortedData,
+    dataSorted,
+    $error,
+    fetchDataSideEffect.pending,
+  ])
 
   return (
     <RootLayout page="catalogue" title="Catalogue">
@@ -66,7 +39,7 @@ const CataloguePage = () => {
           direction={{ base: 'column', xl: 'row' }}
           mb={{ base: 10, xl: '60px' }}
         >
-          <Filters onSort={handleSort} />
+          <Filters onSort={sort} />
           {sortedData ? (
             <PerfumeList data={sortedData} error={error} loading={loading} />
           ) : (
